60bf5b66355e73d440ce7607622b1cf7cf427c54
GOUNAYA Oussama <b179ry@login.axa>
2023-10-25 00:30:32 +0200

    feat: initialiser la page d'authent sans formulaire

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+// It will handle all the logic that we will need to sign in 
+interface UserAuthFormProps {
+
+}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ }) => {
+    return (<div>UserAuthForm</div>)
+}
+
+export default UserAuthForm
